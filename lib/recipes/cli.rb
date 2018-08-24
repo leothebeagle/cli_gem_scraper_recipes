3323b29e0723c1f8584bfcc25437b06240be7340
Aziz Kosber <a.kosber@gmail.com>
2018-08-24 02:06:17 +0000

    stubs out the cli controller with methods and dummy data

--- lib/recipes/cli.rb
+++ lib/recipes/cli.rb
@@ -2,5 +2,17 @@
 class Recipes::CLI
   def call
     puts "Available recipes:"
+    list_recipes
+    menu
   end
+
+  def list_recipes
+    puts "1. Tomato sauce - the trick is to use a little bit of anchovies - recipe-url"
+    puts "2. Omellette - it's all in the technique - recpe-url"
+  end
+
+  def menu
+    puts "Enter the number of the recipe you would like more info on:"
+  end
+
 end
