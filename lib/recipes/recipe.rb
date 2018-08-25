8faf5db00ebf25506de29d1ad8725b40c2c142ed
Aziz Kosber <a.kosber@gmail.com>
2018-08-25 00:59:27 +0000

    instantiates recipes within Recipes::Recipe

--- lib/recipes/recipe.rb
+++ lib/recipes/recipe.rb
@@ -2,7 +2,17 @@
 class Recipes::Recipe
 
   def self.all
-    puts "1. Tomato sauce - the trick is to use a little bit of anchovies - recipe-url"
-    puts "2. Omellette - it's all in the technique - recpe-url"
+    puts "1. Grilled Chicken Sandwhich with Caesar-ish Dressing - Deeply charred mustardy chicken thighs are paired with a crunchy and refreshing fennel-basil slaw for the ultimate summer-night chicken sandwich. - https://www.bonappetit.com/recipe/grilled-chicken-sandwich-with-mustard-mayo"
+    puts "2. Corn Salsa - Whether it’s scooped up with tortilla chips or spooned over a simple fillet of fish, this zippy charred corn salsa is the ultimate peak-August condiment. - https://www.bonappetit.com/recipe/corn-salsa"
+
+    recipe_1 = self.new
+    recipe_1.name = "Grilled Chicken Sandwhich with Caesar-ish Dressing"
+    recipe_1.description = "Deeply charred mustardy chicken thighs are paired with a crunchy and refreshing fennel-basil slaw for the ultimate summer-night chicken sandwich."
+    recipe_1.recipe_url = "https://www.bonappetit.com/recipe/grilled-chicken-sandwich-with-mustard-mayo"
+
+    recipe_2 = self.new
+    recipe_2.name = "Corn Salsa"
+    recipe_2.description = "Whether it’s scooped up with tortilla chips or spooned over a simple fillet of fish, this zippy charred corn salsa is the ultimate peak-August condiment."
+    recipe_2.recipe_url = "https://www.bonappetit.com/recipe/corn-salsa"
   end
 end
