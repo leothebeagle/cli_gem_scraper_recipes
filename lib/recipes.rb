6665f8a3135629cb6b85ed5d435de4e0c5c4ca03
Aziz Kosber <a.kosber@gmail.com>
2018-08-25 02:12:41 +0000

    adds nokogiri and pry dependencies

--- lib/recipes.rb
+++ lib/recipes.rb
@@ -2,6 +2,8 @@
 require_relative './recipes/version'
 require_relative './recipes/cli'
 require_relative './recipes/recipe'
+require 'nokogiri'
+require 'pry'
 
 module Recipes
   # Your code goes here...
