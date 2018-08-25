8109854d4895726640c7054988e59959f583c059
Aziz Kosber <a.kosber@gmail.com>
2018-08-25 00:25:35 +0000

    includes recipes/recipe dependency in environment file

--- lib/recipes.rb
+++ lib/recipes.rb
@@ -1,6 +1,7 @@
 #acts as environment file / single inheritance point to load dependencies
 require_relative './recipes/version'
 require_relative './recipes/cli'
+require_relative './recipes/recipe'
 
 module Recipes
   # Your code goes here...
